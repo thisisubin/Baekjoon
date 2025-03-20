3ce0fbb2c3897b696b3a9bd51a9eb911a8ce0285
thisisubin <127410970+thisisubin@users.noreply.github.com>
2025-03-20 13:21:32 +0900

    [Bronze V] Title: 사칙연산, Time: 176 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+        System.out.println(num1 % num2);
+    }
+}
